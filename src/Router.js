63eea300aa48081c50eea801a4557945b2897d09
sylee <soyeun.lee5930@gmail.com>
2023-09-12 16:11:28 +0900

    product(create post) page layout

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
+import Product from './pages/ProductList/Product/Product';
 
 const Router = () => {
   return (
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/ProductList/Product/Product.js
+++ src/pages/ProductList/Product/Product.js
@@ -1,5 +1,89 @@
+import React, { useState } from 'react';
+import './Product.scss';
+import { useNavigate } from 'react-router-dom';
+
 const Product = () => {
-  return <div>Product</div>;
+  const navigate = useNavigate();
+
+  const handleCancel = () => {
+    navigate('/productList');
+  };
+
+  // const today = new Date();
+
+  const [post, setPost] = useState({
+    // nickname: '',
+    content: '',
+    // createAt: '',
+  });
+
+  const { content, createAt } = post;
+
+  const handlePostInfo = event => {
+    const { name, value } = event.target;
+
+    setPost({ ...post, [name]: value });
+  };
+
+  // console.log(post);
+
+  const handlePosting = event => {
+    event.preventDefault();
+
+    fetch('http://localhost:8000/post/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: localStorage.getItem('login-token'),
+      },
+      body: JSON.stringify({
+        content: content,
+        createAt: createAt,
+      }),
+    })
+      .then(req => req.json())
+      .then(data => {
+        if (data.status === 200) {
+          alert('등록되었습니다.');
+          navigate('/productList');
+        }
+      });
+  };
+
+  return (
+    <div className="product">
+      <div className="productBody">
+        <form className="inputForm">
+          <div className="publish">
+            <div className="container">
+              <div className="profileImg">
+                <div className="image" />
+              </div>
+              <div className="contentDiv">
+                <div className="nickname">Name</div>
+                <textarea
+                  name="content"
+                  className="content"
+                  placeholder="스레드를 시작하세요."
+                  onChange={handlePostInfo}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="btnBox">
+            <div className="btnline">
+              <button className="cancel" onClick={handleCancel}>
+                취소
+              </button>
+              <button className="posting" onClick={handlePosting}>
+                게시
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
